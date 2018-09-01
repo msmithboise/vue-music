6ea3b380869479a35a64dcb4392784649d67bb57
Michael Smith <msmithboise@gmail.com>
2018-09-01 13:09:40 -0600

    push code

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from './router';
 
 Vue.use(Vuex)
 
+let songApi =  axios.create({
+  baseURL: 'https://itunes.apple.com/search?term=',
+  timeout: 3000,
+})
+
 export default new Vuex.Store({
   state: {
+    songs: [],
+    song: {},
+    playlists: [],
+    playlist: {}
 
   },
   mutations: {
-
+    
   },
   actions: {
-
+  getSongs({commit, dispatch}) {
+    songApi.get('')
+    .then(res => {
+      commit('setSongs', res.data)
+      console.log(res.data)
+    })
+  }
   }
 })
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="home">
 
-
+hello
 
   </div>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  // name: 'home',
-  // components: {
-  //   HelloWorld
-  // }
-}
+  methods: {
+
+
+  },
+  computed: {
+
+  }
+};
 </script>
